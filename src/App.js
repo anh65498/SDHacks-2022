49dbb394b67832a67ce46bb45dd3eedda6a4b171
Gaurang Ruparelia <gr2159@nyu.edu>
2022-04-09 03:09:29 -0400

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import Homepage from "./pages/Homepage/Homepage"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import SignupPage from "./pages/SignupPage/SignupPage"
+import LoginPage from "./pages/LoginPage/LoginPage"
+import SetupPage from "./pages/SetupPage/SetupPage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Homepage />} />
+          <Route exact path='/signup' element={<SignupPage />} />
+          <Route exact path='/login' element={<LoginPage />} />
+          <Route exact path='/setup' element={<SetupPage />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/SetupPage/SetupPage.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const SetupPage = () => {
+  return <div>SetupPage</div>
+}
+
+export default SetupPage
