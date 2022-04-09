0691320d5e2c86ef89424ab8da9de38beba8c055
Anh Pham <anh65498@gmail.com>
2022-04-09 07:27:13 -0700

    Login background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from "./logo.svg"
-import "./App.css"
-import Homepage from "./pages/Homepage/Homepage"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import SignupPage from "./pages/SignupPage/SignupPage"
-import LoginPage from "./pages/LoginPage/LoginPage"
-import SetupPage from "./pages/SetupPage/SetupPage"
+import logo from "./logo.svg";
+import "./App.css";
+import Homepage from "./pages/Homepage/Homepage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SetupPage from "./pages/SetupPage/SetupPage";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/' element={<Homepage />} />
-          <Route exact path='/signup' element={<SignupPage />} />
-          <Route exact path='/login' element={<LoginPage />} />
-          <Route exact path='/setup' element={<SetupPage />} />
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/setup" element={<SetupPage />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React from "react";
+import "./loginpage.css";
+// import Button from "@mui/material/Button";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>
-}
+  return (
+    <React.Fragment>
+      <img src={require("../../photos/login_background.png")} className="bg" />
+      <p>Login</p>
+    </React.Fragment>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
