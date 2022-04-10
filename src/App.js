1b24f7c3ac6db21332c4151882c2aeca9b569d09
Anh Pham <anh65498@gmail.com>
2022-04-09 23:40:11 -0700

    Homepage UI

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SetupPage from "./pages/SetupPage/SetupPage";
+import Result from "./pages/Result/ResultPage";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/setup" element={<SetupPage />} />
+          <Route exact path="/result" element={<Result />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,7 +1,124 @@
-import React from "react"
+import React from "react";
+import Navbar from "../components/Navbar";
+import "./Homepage.css";
 
 const Homepage = () => {
-  return <div>Homepage</div>
-}
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div className="container wrapper">
+        <div id="greetings">
+          <h1>
+            Hey <span class="username">Zach</span>, 👋🏻
+          </h1>
+          <h4>Fill out the following details mentioned ...</h4>
+        </div>
+        <form class="container my-container">
+          {/* location input field */}
+          <div class="row">
+            <div class="col-2">
+              <label for="location" class="col-form-label">
+                Enter your Location
+              </label>
+            </div>
+            <div class="col-4 ">
+              <div class="input-group mb-3">
+                <input
+                  id="location"
+                  type="text"
+                  class="form-control"
+                  placeholder="9450 Gilman Drive, La Jolla, CA 92092-0100"
+                />
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Bu
+                </button>
+              </div>
+            </div>
+            {/*separation*/}
+            <div class="col-2"> </div>
 
-export default Homepage
+            {/*type of soil*/}
+            <div class="col-1">
+              <label for="soil-type" class="col-form-label">
+                Soil Type
+              </label>
+            </div>
+            <div class="col-1">
+              <div class="input-group mb-3">
+                <select class="form-select" id="soil-type">
+                  <option selected>Clay</option>
+                  <option value="1">Loam</option>
+                  <option value="2">Sand</option>
+                  <option value="3">Silt</option>
+                </select>
+              </div>
+            </div>
+          </div>{" "}
+          {/*row */}
+          {/* Crop Type input field */}
+          <div class="row">
+            <div class="col-2">
+              <label for="crop" class="col-form-label">
+                Type of Crop
+              </label>
+            </div>
+            <div class="col-4 ">
+              <div class="input-group mb-3">
+                <input
+                  id="crop"
+                  type="text"
+                  class="form-control"
+                  placeholder="Corn"
+                />
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Bu
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Used Water input field */}
+          <div class="row">
+            <div class="col-2">
+              <label for="water" class="col-form-label">
+                Water Used
+              </label>
+            </div>
+            <div class="col-4 ">
+              <div class="input-group mb-3">
+                <input
+                  id="water"
+                  type="number"
+                  min="0"
+                  class="form-control"
+                  placeholder="10"
+                />
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Bu
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="btn-container">
+            <button type="submit" class="btn btn-lg submit-btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Homepage;
