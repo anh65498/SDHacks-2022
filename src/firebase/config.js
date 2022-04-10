649c6101533348ca89d48741321a3bce28e95c7a
Anh Pham <anh65498@gmail.com>
2022-04-10 10:04:30 -0700

    Delete Firebase API keys until judging is done

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,15 +8,7 @@ import "firebase/compat/firestore";
 // global.XMLHttpRequest = require("xhr2");
 
 // Your web app's Firebase configuration
-let db = firebase.initializeApp({
-  apiKey: "AIzaSyBAUWX7kQFZq8rcMNIWm4SydySupwxCE34",
-  authDomain: "sdhacks-41262.firebaseapp.com",
-  projectId: "sdhacks-41262",
-  storageBucket: "sdhacks-41262.appspot.com",
-  messagingSenderId: "186712232722",
-  appId: "1:186712232722:web:f649e5d0158717434999cb",
-  measurementId: "G-9HFPWDD5YT",
-});
+let db = firebase.initializeApp({});
 
 export const provider2 = new firebase.auth.GoogleAuthProvider();
 
